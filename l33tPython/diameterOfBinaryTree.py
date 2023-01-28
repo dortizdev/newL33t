dcae3489bf1362f7d8f2bff9a30415a56319f594
Daniel Ortiz <danielortiz08@gmail.com>
2023-01-27 22:32:25 -0500

    Add python solution for diameter of binary tree

---
+++ l33tPython/diameterOfBinaryTree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.width = 0
+            
+        def dfs(root):
+            if not root: return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            self.width = max(self.width, (left + right))
+            
+            return max(left, right) + 1
+        
+        dfs(root)
+        return self.width
