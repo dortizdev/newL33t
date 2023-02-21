2053dc9e558210f345e008010787750662c6724a
Daniel Ortiz <danielortiz08@gmail.com>
2023-02-20 19:46:33 -0500

    Add python solution for insert interval

---
+++ l33tPython/insertInterval.py
@@ -0,0 +1,15 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        mergedInterval = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                mergedInterval.append(newInterval)
+                return mergedInterval + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                mergedInterval.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0],intervals[i][0]), max(newInterval[1],intervals[i][1])]
+            
+        mergedInterval.append(newInterval)
+        return mergedInterval
