0a643d3457a4f58c9f0b493bb5072eac90dac000
Daniel Ortiz <danielortiz08@gmail.com>
2023-07-14 20:35:40 -0400

    counting bits python solution

---
+++ l33tPython/countingBits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits = [0] * (n+1)
+
+        for i in range(1, n+1):
+            bits[i] = bits[i>>1] + (i%2)
+
+        return bits
